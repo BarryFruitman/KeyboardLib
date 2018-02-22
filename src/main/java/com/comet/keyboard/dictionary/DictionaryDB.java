b35d68d9b80bbae1032e7098bbba730254c70bdb
Barry Fruitman <barry@fruitman.me>
2018-02-21 16:40:31 -0800

    Added db upgrade from UserDB.

--- src/main/java/com/comet/keyboard/dictionary/DictionaryDB.java
+++ src/main/java/com/comet/keyboard/dictionary/DictionaryDB.java
@@ -1,19 +1,25 @@
 package com.comet.keyboard.dictionary;
 
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 public abstract class DictionaryDB {
 
-	static LexiconDbOpenHelper mOpenHelper = null;
+	LexiconDbOpenHelper mOpenHelper = null;
 
-	DictionaryDB(Context context, String language) {
+
+	DictionaryDB(final Context context, final String language) {
 		mOpenHelper = getOpenHelper(context, language);
 	}
 	
-	private static synchronized LexiconDbOpenHelper getOpenHelper(Context context, String language) {
+
+	private synchronized LexiconDbOpenHelper getOpenHelper(
+			final Context context,
+			final String language) {
 		if(mOpenHelper == null || !mOpenHelper.getLanguage().equals(language)) {
 			mOpenHelper = new LexiconDbOpenHelper(context, language);
 		}
@@ -25,16 +31,18 @@ public abstract class DictionaryDB {
 	public abstract int loadDictionaryFromDB(TrieDictionary lexicon, int nRecords);
 
 
-	static class LexiconDbOpenHelper extends SQLiteOpenHelper {
-
+	class LexiconDbOpenHelper extends SQLiteOpenHelper {
 		private static final String DB_EXT = ".dic";
 		private static final int DB_VERSION = 2;
-		private static String mLanguage;
-		private static int mReferenceCount = 0;
+		private String mLanguage;
+		private int mReferenceCount = 0;
+		private final Context mContext;
 
-		private LexiconDbOpenHelper(Context context, String language) {
+
+		private LexiconDbOpenHelper(final Context context, final String language) {
 			super(context, language + DB_EXT, null, DB_VERSION);
-			
+
+			mContext = context;
 			mLanguage = language;
 		}
 		
@@ -60,19 +68,89 @@ public abstract class DictionaryDB {
 
 
 		@Override
-		public void onCreate(SQLiteDatabase db) {
+		public void onCreate(final SQLiteDatabase db) {
 			// Do nothing
 		}
 
 		
 		@Override
-		public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-			if(oldVersion < 2) {
-				// Copy UserDB to main db.
-
-
-				// Delete userdb.
+		public void onUpgrade(final SQLiteDatabase db,
+							  final int oldVersion,
+							  final int newVersion) {
+			if(oldVersion == 1 && newVersion == 2) {
+				upgradeLanguageToV2(db);
+				upgradeLookAheadToV2(db);
 			}
+		}
+
+
+		private void upgradeLanguageToV2(final SQLiteDatabase db) {
+			// Copy UserDB to main db.
+			final UserDB userDB = new UserDB(mContext, mLanguage);
+			userDB.iterateLanguage(new UserDB.UserDbLanguageIteratorCallback() {
+				@Override
+				public void nextEntry(final String word, final int count) {
+					final Cursor cursor = db.query(
+							"lexicon",
+							new String[] { "count" },
+							"count > 0 AND word=?",
+							new String[] { word },
+							null,
+							null,
+							null);
+
+					// Insert or update
+					if(cursor != null && cursor.getCount() > 0) {
+						final int newCount = cursor.getInt(0) + count;
+						final ContentValues values = new ContentValues();
+						values.put("word", word);
+						values.put("count", newCount);
+						db.update(
+								"lexicon",
+								values,
+								"word=?",
+								new String[] { word });
+					}
+				}
+			});
+		}
+
+
+		private void upgradeLookAheadToV2(final SQLiteDatabase db) {
+			// Copy UserDB to main db.
+			final UserDB userDB = new UserDB(mContext, mLanguage);
+			userDB.iterateLookAhead(new UserDB.UserDbLookAheadIteratorCallback() {
+				@Override
+				public void nextEntry(
+						final String word1,
+						final String word2,
+						final String word3,
+						final int count) {
+					final Cursor cursor = db.query(
+							"lookahead",
+							new String[] { "count" },
+							"count > 0 AND word1=? AND word2=? AND word3=?",
+							new String[] { word1, word2, word3 },
+							null,
+							null,
+							null);
+
+					// Insert or update
+					if(cursor != null && cursor.getCount() > 0) {
+						final int newCount = cursor.getInt(0) + count;
+						final ContentValues values = new ContentValues();
+						values.put("word1", word1);
+						values.put("word2", word2);
+						values.put("word3", word3);
+						values.put("count", newCount);
+						db.update(
+								"lookahead",
+								values,
+								"word1=? AND word2=? AND word3=?",
+								new String[] { word1, word2, word3 });
+					}
+				}
+			});
 		}
 	}
 }
--- src/main/java/com/comet/keyboard/dictionary/UserDB.java
+++ src/main/java/com/comet/keyboard/dictionary/UserDB.java
@@ -1,17 +1,27 @@
 package com.comet.keyboard.dictionary;
 
-import android.content.Context;
-import android.database.sqlite.SQLiteDatabase;
-import android.database.sqlite.SQLiteOpenHelper;
+import com.comet.keyboard.KeyboardApp;
 
+
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+
+/**
+ * This database is deprecated and this class exists only for db upgrade purposes.
+ */
 public final class UserDB {
+
 	private static UserDB mUserDB;
 	protected SQLiteOpenHelper mOpenHelper = null;
 	private String mLanguage = "en";
 
 	// User look-ahead table
 	private static final String LOOKAHEAD_TABLE_NAME = "trigrams";
-	private static final String LOOKAHEAD_FIELD_LANG = "lang";
 	private static final String LOOKAHEAD_FIELD_WORD1 = "word1";
 	private static final String LOOKAHEAD_FIELD_WORD2 = "word2";
 	private static final String LOOKAHEAD_FIELD_WORD3 = "word3";
@@ -19,23 +29,11 @@ public final class UserDB {
 
 	// User lexicon table
 	private static final String LEXICON_TABLE_NAME = "lexicon";
-	private static final String LEXICON_FIELD_LANG = "lang";
 	private static final String LEXICON_FIELD_WORD = "word";
 	private static final String LEXICON_FIELD_COUNT = "count";
 
-	public static synchronized UserDB getUserDB(Context context, String language) {
-		if(mUserDB == null || !mUserDB.mLanguage.equals(language))
-			mUserDB = new UserDB(context, language);
-
-		return mUserDB;
-	}
-
-
-
-	private UserDB(Context context, String language) {
-
+	UserDB(Context context, String language) {
 		mOpenHelper = new UserDbOpenHelper(context);
-		
 		mLanguage = language;
 	}
 
@@ -50,10 +48,102 @@ public final class UserDB {
 		mUserDB.mOpenHelper = null;
 		mUserDB = null;
 	}
-	
+
+
+	/*
+	 * Build trees from tables
+	 */
+
+
+	public interface UserDbLanguageIteratorCallback {
+		void nextEntry(String word, int count);
+	}
+
+
+	/**
+	 * Iterates through every language entry, calling a callback for each
+	 */
+	public final void iterateLanguage(UserDbLanguageIteratorCallback callback) {
+		SQLiteDatabase db = mOpenHelper.getWritableDatabase();
+
+		try {
+			Cursor cursor = db.query(
+					LEXICON_TABLE_NAME,
+					new String[] {
+							LEXICON_FIELD_WORD,
+							LEXICON_FIELD_COUNT
+					},
+					"count > 0 AND lang='" + mLanguage + "'",
+					null,
+					null,
+					null,
+					"count DESC");
+
+			if (cursor == null) {
+				return;
+			}
+
+			while(cursor.moveToNext()) {
+				String word = cursor.getString(0);
+				int count = cursor.getInt(1);
+				callback.nextEntry(word, count);
+			}
+
+			cursor.close();
+
+		} catch (SQLiteException e) {
+			Log.e(KeyboardApp.LOG_TAG, e.getMessage(), e);
+		}
+	}
+
+
+	public interface UserDbLookAheadIteratorCallback {
+		void nextEntry(String word1, String word2, String word3, int count);
+	}
+
+
+	/**
+	 * Iterates through every look-ahead entry, calling a callback for each
+	 */
+	public final void iterateLookAhead(UserDbLookAheadIteratorCallback callback) {
+		SQLiteDatabase db = mOpenHelper.getWritableDatabase();
+
+		try {
+			// Load 3-grams
+			final Cursor cursor = db.query(
+					LOOKAHEAD_TABLE_NAME,
+					new String[] {
+							LOOKAHEAD_FIELD_WORD1,
+							LOOKAHEAD_FIELD_WORD2,
+							LOOKAHEAD_FIELD_WORD3,
+							LOOKAHEAD_FIELD_COUNT },
+					"count >= 0 AND lang='" + mLanguage + "'",
+					null,
+					null,
+					null,
+					"count DESC");
+			if (cursor == null) {
+				return;
+			}
+
+			while(cursor.moveToNext()) {
+				String word1 = cursor.getString(0);
+				String word2 = cursor.getString(1);
+				String word3 = cursor.getString(2);
+				int count = cursor.getInt(3);
+
+				callback.nextEntry(word1, word2, word3, count);
+			}
+
+			cursor.close();
+
+		} catch (SQLiteException e) {
+			Log.e(KeyboardApp.LOG_TAG, e.getMessage(), e);
+		}
+	}
+
 
 	private class UserDbOpenHelper extends SQLiteOpenHelper {
-
 		private static final String DB_FILE = "user_dictionary.db";
 		private static final int DB_VERSION = 2;
 
